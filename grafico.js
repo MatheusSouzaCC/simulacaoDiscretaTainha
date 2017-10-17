d5e1abf807706d21bd17682e8f648844f035c76d
Matheus de Souza <matheussouza@edu.univali.br>
2017-10-17 13:31:46 -0200

    Commit inicial

---
+++ grafico.js
@@ -0,0 +1,25 @@
+var ctx = $("#myChart");
+
+var ctx = document.getElementById("myChart");
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"],
+        datasets: [{
+            label: 'População',
+            data: [100, 80, 90, 70, 50, 20],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
